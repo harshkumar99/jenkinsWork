cf3bae9d3fd55e687c73f301a1fe7c1b0480d400
harshkumar99 <53555378+harshkumar99@users.noreply.github.com>
2020-04-11 13:17:15 +0530

    Create ec2-ubuntu.sh

---
+++ ec2-ubuntu.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+aws ec2 run-instances --image-id ami-07ebfd5b3428b6f4d --count 1 --instance-type t2.micro --key-name n_verginia --security-group-ids sg-019482fb1a05e563e --subnet-id subnet-079f696e5731e3661
