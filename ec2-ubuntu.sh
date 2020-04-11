0d9f1a4158caadeb4d5ce7857f3c750babd2494e
harshkumar99 <53555378+harshkumar99@users.noreply.github.com>
2020-04-11 16:59:12 +0530

    Update ec2-ubuntu.sh

--- ec2-ubuntu.sh
+++ ec2-ubuntu.sh
@@ -4,5 +4,4 @@
 aws ec2 run-instances --image-id ami-07ebfd5b3428b6f4d --count 1 --instance-type t2.micro --key-name n_verginia --security-group-ids sg-019482fb1a05e563e --subnet-id subnet-079f696e5731e3661 --region us-east-1 \ 
 --user-data file://ubuntu.txt
 
-# for ubuntu OS use ubuntu.txt
-# for amazon linux or centos OS use centos.txt
+
