dc9af1940374730133a3a57d99b3bbffbd25b230
harshkumar99 <53555378+harshkumar99@users.noreply.github.com>
2020-04-11 13:54:38 +0530

    Update ec2-ubuntu.sh

--- ec2-ubuntu.sh
+++ ec2-ubuntu.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-aws ec2 run-instances --image-id ami-07ebfd5b3428b6f4d --count 1 --instance-type t2.micro --key-name n_verginia --security-group-ids sg-019482fb1a05e563e --subnet-id subnet-079f696e5731e3661
+aws ec2 run-instances --image-id ami-07ebfd5b3428b6f4d --count 1 --instance-type t2.micro --key-name n_verginia --security-group-ids sg-019482fb1a05e563e --subnet-id subnet-079f696e5731e3661 --region us-east-1
